022dc7d8bfadbbc2000b2563b0e2fafa94b44348
Har0l <haroldurrea6@gmail.com>
2024-02-11 21:03:11 -0500

    Feat: Data structures done

--- src/data.js
+++ src/data.js
@@ -1,26 +1,43 @@
-export const cards = [
+export const Cards = [
   {
-    title: "Card 1",
-    description: "lorem ipsum",
-    src: "https://picsum.photos/200/300",
-    color: "#F0F0F0",
+    title: "Feliz cumpleaños, mi mari chiquita",
+    description: "Hoy es tu dia especial, quiero recordarte la increible persona que eres y que seas la mujer mas feliz.",
+    src: "First.jpeg",
+    link: "https://www.youtube.com/watch?v=jqYxyd1iSNk&list=PLOBMuur4EBvTuVUkSEqsCOXf0y6wQZBjk&index=3",
+    color: "#1B3E59",
+    textColor: "#FFFFFF"
   },
   {
-    title: "Card 1",
-    description: "lorem ipsum",
-    src: "https://picsum.photos/200/300",
-    color: "#F0F0F0",
+    title: "Solo tu <3",
+    description: "Podrias sacarle a cualquier persona una sonrisa, es algo que te sale natural y es una de las mejores cualidades que tienes, que bonito es verte crecer y seguir sonriendo.",
+    src: "Second.jpeg",
+    link: "https://www.youtube.com/watch?v=kvazBqAlx58",
+    color: "#F2F0F0",
+    textColor: "#000000"
   },
   {
-    title: "Card 1",
-    description: "lorem ipsum",
-    src: "https://picsum.photos/200/300",
-    color: "#F0F0F0",
+    title: "Mereces lo mejor",
+    description: "Y solo lo mejor en tu vida, que Dios te siga bendiciendo con tantas oportunidades y que siempre recuerdes que eres una gran mujer.",
+    src: "Third.jpeg",
+    link: "https://www.youtube.com/watch?v=nfWlot6h_JM",
+    color: "#FFAC00",
+    textColor: "#000000"
   },
   {
-    title: "Card 1",
-    description: "lorem ipsum",
-    src: "https://picsum.photos/200/300",
-    color: "#F0F0F0",
+    title: "En este Dia",
+    description: "Quiero recordarte lo feliz que me haces, lo grandiosa que eres y que sepas que tengo las espectativas muy altas contigo, porque solo tu podrias ser tan bella, talentosa y gentil todo el tiempo.",
+    src: "Fourth.jpeg",
+    link: "https://www.youtube.com/watch?v=8D4hcrkI2xU",
+    color: "#BF0404",
+    textColor: "#FFFFFF"
   },
-];
+  {
+    title: "y soy yo :p",
+    description: "El que te va a decir que sigas adelante, que tienes inmensas cosas por lograr y que tu futuro se ve brillante incluso desde mi punto de vista, Te amo.",
+    src: "Fifth.jpeg",
+    link: "https://www.youtube.com/watch?v=9XUw4wvmRps",
+    color: "#730202",
+    textColor: "#FFFFFF"
+  },
+
+]
