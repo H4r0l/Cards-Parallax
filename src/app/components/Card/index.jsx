5be496d6db36976fb1ebc18e0e1808323adf035e
Har0l <haroldurrea6@gmail.com>
2024-02-14 21:37:11 -0500

    Fix: 'a' element ugly underline removed ;c

--- src/app/components/Card/index.jsx
+++ src/app/components/Card/index.jsx
@@ -1,6 +1,7 @@
 'use client'
 import Image from 'next/image';
 import styles from './style.module.scss';
+import "./extra.styles.css"
 import { useTransform, motion, useScroll } from 'framer-motion';
 import { useRef } from 'react';
 
